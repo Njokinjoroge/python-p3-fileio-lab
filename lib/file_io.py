668f135ae68e170502c97536c8d756f7d166db30
MorganNjoroge <zeejaymorgan@morgan.com>
2024-03-18 11:35:33 +0300

    mycommit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
-def write_file(file_name, file_content):
-    pass
+import os
 
-def append_file(file_name, append_content):
-    pass
+def write_file(file_name, file_content):
+    file_name_with_extension = str(file_name) + ".txt"
+    with open(file_name_with_extension, "w") as f:
+        f.write(file_content)
+
+def append_file(file_name, file_content):
+    file_name_with_extension = str(file_name) + ".txt"
+    with open(file_name_with_extension, "a") as f:
+        f.write(file_content)
 
 def read_file(file_name):
-    pass
+    file_name_with_extension = str(file_name) + ".txt"
+    if os.path.isfile(file_name_with_extension):
+        with open(file_name_with_extension, "r") as f:
+            file_content = f.read()
+        return file_content
+    else:
+        return None
